d8b0e238db650a3f886bbcf051cbb0dfe0568c32
Eko Pandu N <89683286+ekopandu@users.noreply.github.com>
2021-09-30 21:25:52 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Eko</li>
+ <li>Nama Belakang : Pandu</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan haji kasam </li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Kebumen</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>futsal</li>
+ </ul>
+</secti
